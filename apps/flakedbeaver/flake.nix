327673008fac8c0090a482437900f3ed9d4907bd
kei19 <kitoyu22@gmail.com>
2025-07-19 09:47:06 +0700

    Add working flake for dbeaver

--- apps/flakedbeaver/flake.nix
+++ apps/flakedbeaver/flake.nix
@@ -13,46 +13,13 @@
           inherit system;
         };
       in {
-        # For `nix run .`
         apps.default = {
           type = "app";
           program = "${pkgs.dbeaver-bin}/bin/dbeaver";
         };
 
-        # For `nix build .#dbeaver` or `nix profile install .#dbeaver`
         packages.dbeaver = pkgs.dbeaver-bin;
 
-        # For `nix develop`
-        devShells.default = pkgs.mkShell {
-          packages = [ pkgs.dbeaver-bin ];
-        };
-      });
-}
-{
-  description = "Flake that provides DBeaver";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in {
-        # For `nix run .`
-        apps.default = {
-          type = "app";
-          program = "${pkgs.dbeaver-bin}/bin/dbeaver";
-        };
-
-        # For `nix build .#dbeaver` or `nix profile install .#dbeaver`
-        packages.dbeaver = pkgs.dbeaver-bin;
-
-        # For `nix develop`
         devShells.default = pkgs.mkShell {
           packages = [ pkgs.dbeaver-bin ];
         };
