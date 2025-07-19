bba53c89a8a1518efbf7585b53a67dac7edb57f1
kei19 <kitoyu22@gmail.com>
2025-07-19 09:54:46 +0700

    Add DBeaver flake

--- apps/flakedbeaver/flake.nix
+++ apps/flakedbeaver/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Flake that provides DBeaver";
+  description = "Build a macOS .app bundle for DBeaver";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
@@ -9,19 +9,48 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
+        pkgs = import nixpkgs { inherit system; };
+
+        dbeaver = pkgs.dbeaver-bin;
+
+        dbeaverApp = pkgs.stdenv.mkDerivation {
+          pname = "DBeaver";
+          version = "23.3"; # or whatever version nixpkgs has
+
+          buildInputs = [ pkgs.makeWrapper ];
+
+          unpackPhase = "true";
+
+          installPhase = ''
+            mkdir -p $out/Applications/DBeaver.app/Contents/MacOS
+            mkdir -p $out/Applications/DBeaver.app/Contents/Resources
+
+            # Launcher script
+            makeWrapper ${dbeaver}/bin/dbeaver $out/Applications/DBeaver.app/Contents/MacOS/DBeaver
+
+            # Optional: minimal Info.plist for macOS
+            cat > $out/Applications/DBeaver.app/Contents/Info.plist <<EOF
+            <?xml version="1.0" encoding="UTF-8"?>
+            <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
+              "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+            <plist version="1.0">
+              <dict>
+                <key>CFBundleName</key>
+                <string>DBeaver</string>
+                <key>CFBundleExecutable</key>
+                <string>DBeaver</string>
+                <key>CFBundleIdentifier</key>
+                <string>org.dbeaver.app</string>
+                <key>CFBundleVersion</key>
+                <string>${dbeaver.version}</string>
+                <key>CFBundlePackageType</key>
+                <string>APPL</string>
+              </dict>
+            </plist>
+            EOF
+          '';
         };
       in {
-        apps.default = {
-          type = "app";
-          program = "${pkgs.dbeaver-bin}/bin/dbeaver";
-        };
-
-        packages.dbeaver = pkgs.dbeaver-bin;
-
-        devShells.default = pkgs.mkShell {
-          packages = [ pkgs.dbeaver-bin ];
-        };
+        packages.default = dbeaverApp;
       });
 }
