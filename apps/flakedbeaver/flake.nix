fe4e6d59adb7bbb07878a51399db704c39846628
kei19 <kitoyu22@gmail.com>
2025-07-19 09:46:13 +0700

    Add working flake for dbeaver

--- apps/flakedbeaver/flake.nix
+++ apps/flakedbeaver/flake.nix
@@ -2,7 +2,7 @@
   description = "Flake that provides DBeaver";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05"; # or unstable
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -16,15 +16,45 @@
         # For `nix run .`
         apps.default = {
           type = "app";
-          program = "${pkgs.dbeaver}/bin/dbeaver";
+          program = "${pkgs.dbeaver-bin}/bin/dbeaver";
         };
 
         # For `nix build .#dbeaver` or `nix profile install .#dbeaver`
-        packages.dbeaver = pkgs.dbeaver;
+        packages.dbeaver = pkgs.dbeaver-bin;
 
         # For `nix develop`
         devShells.default = pkgs.mkShell {
-          packages = [ pkgs.dbeaver ];
+          packages = [ pkgs.dbeaver-bin ];
+        };
+      });
+}
+{
+  description = "Flake that provides DBeaver";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        # For `nix run .`
+        apps.default = {
+          type = "app";
+          program = "${pkgs.dbeaver-bin}/bin/dbeaver";
+        };
+
+        # For `nix build .#dbeaver` or `nix profile install .#dbeaver`
+        packages.dbeaver = pkgs.dbeaver-bin;
+
+        # For `nix develop`
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.dbeaver-bin ];
         };
       });
 }
