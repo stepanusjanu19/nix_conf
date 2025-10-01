cfa0db2e0abbd4fd8749012e23d24fb834ee0c0d
stepanusjanu19 <kitoyu22@gmail.com>
2025-10-01 13:19:25 +0700

    feat : add configure tkinter pkgs

--- apps/tkinter/flake.nix
+++ apps/tkinter/flake.nix
@@ -1,21 +1,24 @@
 {
-  description = "Python + Tkinter devshell";
+  description = "DevShell with Python + Tkinter + Mamba";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = {
     self,
     nixpkgs,
-  }: {
-    devShells.default = nixpkgs.lib.mkShell {
+  }: let
+    system = "x86_64-darwin"; # or "aarch64-darwin" if Apple Silicon
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
       buildInputs = [
-        (nixpkgs.python3.withPackages (ps:
+        (pkgs.python3.withPackages (ps:
           with ps; [
             tkinter
             numpy
             pandas
           ]))
-        nixpkgs.mambaforge
+        pkgs.mambaforge
       ];
     };
   };
